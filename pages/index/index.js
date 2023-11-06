1bb5f0730bfde004b86bdb0ee365a72633f31083
刘金雨 <332183899@qq.com>
2023-11-06 10:31:55 +0800

    11.6second_modification

--- pages/index/index.js
+++ pages/index/index.js
@@ -45,73 +45,6 @@ Page({
       hasUserInfo: true
     })
   },
-<<<<<<< HEAD
-  next_calculator:function()
-{
-  wx.navigateTo({
-    url: '/pages/signature',
-  })
-}, loadImage() {
-  let that = this;
-  wx.chooseImage({
-    count: 0,
-    sizeType: ['original', 'compressed'], //原图 / 压缩
-    sourceType: ['album', 'camera'], //相册 / 相机拍照模式
-    success(res) {
-      that.setData({
-        imgSrc: res.tempFilePaths[0]
-      });
-      //将图片转换为Base64格式
-      // wx.getFileSystemManager().readFile({
-      //   filePath: res.tempFilePaths[0],
-      //   //encoding: 'base64',
-      //   success(data) {
-      //     let baseData = data.data; //'data:image/png;base64,' + data.data;
-      //     that.setData({
-      //       baseData: baseData
-      //     });
-      //   }
-      // });
-    }
-  })
-},
-identify() {
-  let that = this;
-  // let requestData = {
-  //   'image': that.data.baseData,
-  //   'imge_type':BASE64
-  // };
-  //调用接口
-  // wx.request({
-  //   url: 'https://localhost:8080/use/image', //动物
-  //   method: 'POST',
-  //   header: {
-  //     'content-type': 'application/x-www-form-urlencoded'
-  //   },
-  //   data: requestData,
-  //   success: function (identify) {
-  //     that.setData({
-  //       isShowDetail: true,
-  //       resultList: identify.data.result
-  //     });
-  //   }
-  // })
-  wx.uploadFile({
-    url:'http://localhost:8080/use/image', //接受图片的接口地址
-    filePath: that.data.imgSrc,
-    name: 'file',
-    formData: {
-        'postfix': '.png'
-    },
-    success (res){
-        console.log(res);
-        const data_str = res.data;
-        const data = JSON.parse(data_str)
-        //do something
-         // wx.navigateTo({url: '/pages/identify',})
-         wx.navigateTo({url: '/pages/identify?image='+data.file+"&text="+data.text,})
-        }
-=======
   loadImage() {
     let that = this;
     wx.chooseImage({
@@ -123,7 +56,6 @@ identify() {
           imgSrc: res.tempFilePaths[0]
         }); 
       }
->>>>>>> 22693fb (11.6modification)
     })
   },
   identify() {
